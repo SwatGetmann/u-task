3b08f0acf6767e525b9ee682d2c6971fb22c56f8
SwatGetmann <swatgetmann@gmail.com>
2015-09-08 12:09:25 +0300

    deleted rating from tasks form, validation for tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,10 @@
 class Task < ActiveRecord::Base
+	validates :name, 	presence: true,
+		uniqueness: true
+	validates :description, presence:true
+	validates :level, presence: true,
+		numericality: { only_integer: true, in: 0..5 }
+	validates :category, presence: true,
+		inclusion: { in: %w(C++ Java Python Ruby),
+		message: "must be C++ || Java | Python | Ruby" }
 end
