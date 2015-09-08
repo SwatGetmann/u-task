9fc3aae2db72649e18b92ea48eb622d9340d5f03
SwatGetmann <swatgetmann@gmail.com>
2015-09-08 13:14:50 +0300

    default levels created

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150908094749) do
+ActiveRecord::Schema.define(version: 20150908101038) do
 
   create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "levels", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -22,11 +28,11 @@ ActiveRecord::Schema.define(version: 20150908094749) do
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
-    t.integer  "level"
     t.float    "rating"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "category_id"
+    t.integer  "level_id"
   end
 
   create_table "users", force: :cascade do |t|
