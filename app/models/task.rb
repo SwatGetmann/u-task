5fe5ca4215798eda47a8ec39058b88e8f1f40d88
SwatGetmann <swatgetmann@gmail.com>
2015-09-09 00:00:00 +0300

    fixed error with deleting level and showing it in tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,4 +5,9 @@ class Task < ActiveRecord::Base
 	# validates :category, presence: true
 	belongs_to :category
 	belongs_to :level
+
+	def show_level
+		self.level = Level.last if self.level.nil?
+		self.level.name
+	end
 end
