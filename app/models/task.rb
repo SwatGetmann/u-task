5a577271bfe0d6ba83cc544d4d34149d16a7e358
SwatGetmann <swatgetmann@gmail.com>
2015-09-09 17:51:15 +0300

    fix default level & category

--- app/models/task.rb
+++ app/models/task.rb
@@ -6,13 +6,21 @@ class Task < ActiveRecord::Base
 	belongs_to :category
 	belongs_to :level
 
+	def default_level
+		return Level.new(name: "NO LEVEL, PLEASE EDIT")
+	end
+
+	def default_category
+		return Category.new(name: "NO CATEGORY, PLEASE EDIT")
+	end
+
 	def show_level
-		self.level = Level.last if self.level.nil?
+		self.level = default_level if self.level.nil?
 		self.level.name
 	end
 
 	def show_category
-		self.category = Category.last if self.category.nil?
+		self.category = default_category if self.category.nil?
 		self.category.name
 	end
 end
