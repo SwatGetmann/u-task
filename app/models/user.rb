ec2a9ba58b797866936cab2e3a7b4263a0da085a
SwatGetmann <swatgetmann@gmail.com>
2015-09-09 18:06:38 +0300

    validate password (presence, length)

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,5 +10,5 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :name, presence: true
   validates :email, presence: true, email: true
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
