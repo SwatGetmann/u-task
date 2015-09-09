23ec8ce23ad6e0c82cc3e1a2cd730317f1ce42e3
SwatGetmann <swatgetmann@gmail.com>
2015-09-09 18:03:28 +0300

    validate email (simple)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
+class EmailValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, value)
+    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+      record.errors[attribute] << (options[:message] || "is not an email")
+    end
+  end
+end
+
 class User < ActiveRecord::Base
   has_secure_password
   validates :name, presence: true
-  validates :email, presence:true
+  validates :email, presence: true, email: true
+  validates :password, presence: true
 end
