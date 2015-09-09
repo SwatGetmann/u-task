9c1f8d10b995c737c2b869c57591a73b7f4e490c
SwatGetmann <swatgetmann@gmail.com>
2015-09-09 21:47:31 +0300

    half-way w github auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,15 +9,16 @@ end
 class User < ActiveRecord::Base
   has_secure_password
   validates :name, presence: true
-  validates :email, presence: true, email: true
+  # validates :email, presence: true, email: true
   validates :password, presence: true, length: { minimum: 6 }
 
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.name = auth["user_info"]["name"]
-      user.email = auth["user_info"]["email"]
+      user.name = auth["info"]["name"]
+      user.email = auth["info"]["email"]
+      user.password = SecureRandom.urlsafe_base64(n=6)
     end
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-  provider :vkontakte, ENV['API_KEY'], ENV['API_SECRET']
+  provider :vkontakte, ENV['VK_KEY'], ENV['VK_SECRET']
 end
