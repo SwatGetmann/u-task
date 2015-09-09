9545c0deebb44d6cdc33ef023efa5f2cb65d5b0d
SwatGetmann <swatgetmann@gmail.com>
2015-09-09 22:30:53 +0300

    patch github & vk auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   
   private
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+   	@current_user ||= User.find(session[:user_id]) if !User.where(:id => session[:user_id]).empty?
   end
 end
