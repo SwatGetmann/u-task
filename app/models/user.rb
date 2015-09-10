e981cf98945f206d531feaa0b15ca8709d1f3d42
SwatGetmann <swatgetmann@gmail.com>
2015-09-10 17:26:12 +0300

    layout & route fix sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ end
 class User < ActiveRecord::Base
   has_secure_password
   validates :name, presence: true
-  # validates :email, presence: true, email: true
+  validates :email, presence: true, email: true
   validates :password, presence: true, length: { minimum: 6 }
 
   def self.create_with_omniauth(auth)
@@ -17,8 +17,8 @@ class User < ActiveRecord::Base
       user.provider = auth["provider"]
       user.uid = auth["uid"]
       user.name = auth["info"]["name"]
-      user.email = auth["info"]["email"]
       user.password = SecureRandom.urlsafe_base64(n=6)
+      user.rating = 0.0
     end
   end
 end
