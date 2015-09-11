82973562d9bb59f8f3e242256491a7eed5c5e146
SwatGetmann <swatgetmann@gmail.com>
2015-09-11 14:01:48 +0300

    fix edit provided user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,7 +14,7 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
-    redirect_to edit_user_path(id: user.id), :notice => "Signed in!"
+    redirect_to edit_user_path(id: user.id), :notice => "Please edit your password and email"
   end
 
 	def destroy
