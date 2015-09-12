2d0922c7ac708276f6274cd0264b9f825f7ac644
SwatGetmann <swatgetmann@gmail.com>
2015-09-12 14:43:25 +0300

    fix see tasks by category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -10,6 +10,7 @@ class CategoriesController < ApplicationController
   # GET /categories/1
   # GET /categories/1.json
   def show
+    @tasks = Task.where(:category => @category)
   end
 
   # GET /categories/new
