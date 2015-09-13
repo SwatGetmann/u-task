5edbb0735cddb4f26f7d9348df3d184d433a63fb
SwatGetmann <swatgetmann@gmail.com>
2015-09-13 13:06:09 +0300

    fix failure auth exception

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :vkontakte, ENV['VK_KEY'], ENV['VK_SECRET']
   provider :identity, on_failed_registration: lambda { |env|    
-    IdentitiesController.action(:new).call(env)
+    IdentitiesController.action(:new).call(env) }
+  OmniAuth.config.on_failure = Proc.new { |env|
+  	OmniAuth::FailureEndpoint.new(env).redirect_to_failure }
 end
