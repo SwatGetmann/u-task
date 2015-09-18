7157c7ba5b5319446db3cb55630b2f8e13e3d9ad
SwatGetmann <swatgetmann@gmail.com>
2015-09-18 08:53:44 +0300

    Add answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,7 +1,25 @@
 class AnswersController < ApplicationController
-  def create
+  def new
+  	@task = Task.find(params[:task_id])
+  	@user = User.find(session[:user_id])
+  	@answer = Answer.new(:task => @task, :user => @user, :correct => false)
   end
 
-  def new
+  def create
+  	@answer = Answer.new(:task => @task, :user => @user, :correct => false)
+  	@answer.update(answer_params)
+  	@asnwer.user = @user
+  	@answer.task = @task
+  	if @book.save
+      redirect_to root
+    else
+      render :action => 'new'
+    end
   end
+
+  private
+
+	  def answer_params
+	    params.require(:answer).permit(:content, :user, :task)
+	  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :tasks
   end
+  resources :answers
 
 
   get 'admin' => 'admin#index'
