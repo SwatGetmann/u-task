1298cb7b12db939756473ce98beda3aed364778e
SwatGetmann <swatgetmann@gmail.com>
2015-09-21 09:29:04 +0300

    Small code refactoring

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,8 +1,4 @@
 class Category < ActiveRecord::Base
 	has_many :tasks
 	validates :name, presence: true
-
-	def default_category
-		return Category.new(name: "NO CATEGORY, PLEASE EDIT")
-	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,7 @@
-class EmailValidator < ActiveModel::EachValidator
-  def validate_each(record, attribute, value)
-    unless value =~ /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
-      record.errors[attribute] << (options[:message] || "is not an email")
-    end
-  end
-end
-
 class User < ActiveRecord::Base
   has_many :tasks
   has_many :answers
   has_secure_password
-  
-  # validates :name, presence: true
-  # validates :email, presence: true, email: true
-  # validates :password, presence: true, length: { minimum: 6 }
 
   def self.from_omniauth(auth)
     find_by_provider_and_uid(auth["provider"], auth["uid"]) || create_with_omniauth(auth)
